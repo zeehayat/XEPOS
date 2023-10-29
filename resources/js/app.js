4e7c52844a1e0038a262912223e867559ef943c7
Zeeshan <zeenux@gmail.com>
2023-10-29 13:23:24 +0500

    Fixing Routes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import './bootstrap';
-import {createApp} from 'vue'
-import { createRouter, createWebHistory } from 'vue-router';
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import store from './store/index.js'
 import App from './App.vue'
 import Employee from './components/Employee.vue'
@@ -12,52 +12,64 @@ import ListCompany from './components/ListCompany.vue'
 import Dashboard from './components/Dashboard.vue'
 import Login from './components/Login.vue'
 const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', component: App },
-    { path: '/company', component: Company,meta:{
-            requiresAuth:true
-        } },
-    { path: '/employee', component: Employee,meta:{
-            requiresAuth:true
-        } },
-    {path:'/employees',component: EmployeeList,meta:{
-            requiresAuth:true
-        }},
-    { path: '/empeit/:id', component: EditEmployee,meta:{
-            requiresAuth:true
-        } },
-    { path: '/companies/', component: ListCompany,
-meta:{
-            requiresAuth:true
-        } },
-    { path: '/companyedit/:id', component: EditCompany,
-meta:{
-            requiresAuth:true
-        } },
-    {path:'/login', name: 'login', component:Login,
-},
-    {path:'/', component:Login},
-    {
-        path:'/dashboard',name:'dashboard', component:Dashboard,
-        meta:{
-            requiresAuth:true
-        }
-    },
-  ],
+    history: createWebHistory(),
+    routes: [
+        { path: '/', component: App },
+        {
+            path: '/company', component: Company, meta: {
+                requiresAuth: true
+            }
+        },
+        {
+            path: '/employee', component: Employee, meta: {
+                requiresAuth: true
+            }
+        },
+        {
+            path: '/employees', component: EmployeeList, meta: {
+                requiresAuth: true
+            }
+        },
+        {
+            path: '/empeit/:id', component: EditEmployee, meta: {
+                requiresAuth: true
+            }
+        },
+        {
+            path: '/companies/', component: ListCompany,
+            meta: {
+                requiresAuth: true
+            }
+        },
+        {
+            path: '/companyedit/:id', component: EditCompany,
+            meta: {
+                requiresAuth: true
+            }
+        },
+        {
+            path: '/login', name: 'login', component: Login,
+        },
+        { path: '/', component: Login },
+        {
+            path: '/dashboard', name: 'dashboard', component: Dashboard,
+            meta: {
+                requiresAuth: true
+            }
+        },
+    ],
 });
-router.beforeEach((to,from) => {
-    if(to.meta.requiresAuth && !localStorage.getItem('token'))
-    {
-        return {name: 'login'}
+router.beforeEach((to, from) => {
+    if (to.meta.requiresAuth && !localStorage.getItem('token')) {
+        return { name: 'login' }
     }
-    if(to.meta.requiresAuth==false && localStorage.getItem('token')){
-        return {name: 'dashboard'}
+    if (to.meta.requiresAuth == false && localStorage.getItem('token')) {
+        return { name: 'dashboard' }
     }
 
 })
 export default router
-createApp(App).use(router,store).mount("#app")
+createApp(App).use(router, store).mount("#app")
 
 
 
---
+++ resources/js/router/index.js
@@ -0,0 +1,47 @@
+import { createRouter, createWebHistory } from 'vue-router'
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: App },
+    { path: '/company', component: Company,meta:{
+            requiresAuth:true
+        } },
+    { path: '/employee', component: Employee,meta:{
+            requiresAuth:true
+        } },
+    {path:'/employees',component: EmployeeList,meta:{
+            requiresAuth:true
+        }},
+    { path: '/empeit/:id', component: EditEmployee,meta:{
+            requiresAuth:true
+        } },
+    { path: '/companies/', component: ListCompany,
+meta:{
+            requiresAuth:true
+        } },
+    { path: '/companyedit/:id', component: EditCompany,
+meta:{
+            requiresAuth:true
+        } },
+    {path:'/login', name: 'login', component:Login,
+},
+    {path:'/', component:Login},
+    {
+        path:'/dashboard',name:'dashboard', component:Dashboard,
+        meta:{
+            requiresAuth:true
+        }
+    },
+  ],
+});
+router.beforeEach((to,from) => {
+    if(to.meta.requiresAuth && !localStorage.getItem('token'))
+    {
+        return {name: 'login'}
+    }
+    if(to.meta.requiresAuth==false && localStorage.getItem('token')){
+        return {name: 'dashboard'}
+    }
+
+})
+export default router
